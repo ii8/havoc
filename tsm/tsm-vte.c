b38c4ec1d3355bafc825e80476fac666f4f5748a
murray <murray.calavera@protonmail.com>
2021-12-14 12:31:43 +0000

    DECRQM should not do a reset

--- tsm/tsm-vte.c
+++ tsm/tsm-vte.c
@@ -1577,7 +1577,6 @@ static void do_csi(struct tsm_vte *vte, uint32_t data)
 			/* DECRQM: Request DEC Private Mode */
 			/* If CSI_WHAT is set, then enable,
 			 * otherwise disable */
-			csi_soft_reset(vte);
 		} else {
 			/* DECSCL: Compatibility Level */
 			/* Sometimes CSI_DQUOTE is set here, too */
